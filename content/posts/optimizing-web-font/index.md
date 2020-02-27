1290dab9f4031e17f11dc383c456bb15344f5f9a
Jang Whemoon <palindrom615@gmail.com>
2020-02-27 10:51:32 +0000

    Restruct blog directory

---
+++ content/posts/optimizing-web-font/index.md
@@ -0,0 +1,8 @@
+---
+layout: post
+title: "정적 사이트에서 한국어 웹 폰트 최적화하기"
+tags: [font, optimization, CJK, "static site" ]
+description: "정적 사이트에서 **구글 폰트를 안쓰고** CJK 폰트 최적화 하기"
+date: "2020-02-27"
+---
+
--- gatsby-config.js
+++ gatsby-config.js
@@ -15,7 +15,7 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        path: `${__dirname}/content/blog`,
+        path: `${__dirname}/content/posts`,
         name: `blog`,
       },
     },
@@ -73,7 +73,7 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        path: `${__dirname}/src/markdown-pages`,
+        path: `${__dirname}/content/pages`,
         name: `markdown-pages`,
       },
     },
