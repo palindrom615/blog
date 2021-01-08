fa76c0633298e50eb6b33ad6cb4feeb46bc49872
Jang Whemoon <palindrom615@gmail.com>
2021-01-09 04:00:47 +0900

    Add ssr on mdx image

--- content/posts/newspaper-typography/typographyCardList.jsx
+++ content/posts/newspaper-typography/typographyCardList.jsx
@@ -1,18 +1,11 @@
 import React from "react"
 
-import TypographyCard from "./typographyCard"
+import Img from "../../../src/components/image"
 
 import resultKor from "./journals.kor/result.json"
 import resultUsa from "./journals.usa/result.json"
 import resultChn from "./journals.chn/result.json"
 import resultJpn from "./journals.jpn/result.json"
-
-const zonePath = {
-  kor: "./journals.kor",
-  usa: "./journals.usa",
-  chn: "./journals.chn",
-  jpn: "./journals.jpn",
-}
 
 const result = {
   kor: resultKor,
@@ -36,7 +29,44 @@ const TypographyCardList = ({ zone }) => {
     >
       {results.map((r) => {
         return (
-          <TypographyCard r={r} key={r.journal} location={zonePath[zone]} />
+          <div
+            style={{
+              flex: "0",
+              fontSize: "0.625rem",
+              minWidth: "240px",
+              margin: "6px",
+            }}
+            key={r.journal}
+          >
+            <span style={{ fontWeight: "bold" }}>{r.journal}</span>
+            <Img
+              async
+              src={`${r.journal}-type.png`}
+              style={{ margin: 0, boxShadow: "0 0 1px 1px lightgrey" }}
+              alt={`${r.journal}의 본문 타이포그래피`}
+            />
+            font-size <span style={{ fontWeight: "bold" }}> {r.fontSize}</span>
+            <br />
+            line-height{" "}
+            <span style={{ fontWeight: "bold" }}> {r.lineHeight}</span>
+            <br />
+            font-family{" "}
+            <span style={{ fontWeight: "bold" }}> {r.fontFamily}</span>
+            {r.letterSpacing !== "normal" && (
+              <>
+                <br />
+                letter-spacing{" "}
+                <span style={{ fontWeight: "bold" }}> {r.letterSpacing}</span>
+              </>
+            )}
+            {r.wordSpacing !== "0px" && (
+              <>
+                <br />
+                word-spacing{" "}
+                <span style={{ fontWeight: "bold" }}> {r.wordSpacing}</span>
+              </>
+            )}
+          </div>
         )
       })}
     </div>
